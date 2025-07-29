14b725d789f56cf6dca469e31100b263974322b9
trudransh <trudranshsingh2003@gmail.com>
2025-07-29 19:42:08 +0530

    Added Requirement for the Project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use chacha20poly1305::{
+    aead::{Aead, KeyInit, Nonce},
+    XChaCha20Poly1305,
+};
+use rand::{rngs::OsRng, RngCore};
+
+// use rand::{rngs::OsRng, RngCore};
+
+struct CryptoEngine {
+    cipher: XChaCha20Poly1305,
+}
+
+
+struct EncryptionKey {
+    key: [u8; 32],
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
